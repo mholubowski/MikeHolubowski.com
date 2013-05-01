cc4d997c3f31e21860b6aba9498b4e48a3b564d5
Mike Holubowski <mike.holubowski@gmail.com>
2013-04-30 18:24:04 -0700

    cleaned up front page - for GSoC

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Mikeholubowski::Application.routes.draw do
 
   match '/projects/alliance'     => 'project#alliance' 
   match '/projects/alliedgreeks' => 'project#alliedgreeks' 
-  match '/projects/ball' => 'project#ball' 
+  match '/projects/ball'         => 'project#ball' 
   match '/projects/efp'          => 'project#efp' 
   match '/projects/followalong'  => 'project#followalong' 
   match '/projects/ivlaundry'    => 'project#ivlaundry' 
