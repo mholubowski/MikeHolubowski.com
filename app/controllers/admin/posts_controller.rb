809989ed1b7a150ce42949d864370c35ad6e3df9
Mike Holubowski <mike.holubowski@gmail.com>
2013-08-23 14:41:19 -0700

    Added button to get to admin; Namespaced posts as admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,13 +1,19 @@
 class Admin::PostsController < ApplicationController
   layout 'who_blogs_anyway'
 
+  def show
+  end
+
+  def index
+  end
+
   def new
     @post = Post.new
   end
 
   def create
     @post = Post.new(params[:post]) do |p|
-      # p.user = current_user
+      p.user = current_user
     end
 
     if @post.save
